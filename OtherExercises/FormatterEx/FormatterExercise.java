ec46443ff8b6374ab1458e2966f2df72905e6742
Mariusz Pacyga <mariusz.pacyga@gmail.com>
2022-01-09 21:13:39 +0100

    formatter exercise

--- OtherExercises/FormatterEx/FormatterExercise.java
+++ OtherExercises/FormatterEx/FormatterExercise.java
@@ -3,15 +3,15 @@ package OtherExercises.FormatterEx;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class FormatterExercise {
 
     public static void main(String args[]) throws IOException {
-
-        String[] data = new String(Files.readAllBytes(Paths.get("src/main/java/OtherExercises/FormatterEx/data.txt"))).split("\\n");
+        List<String> data = Files.readAllLines(Paths.get("src/main/java/OtherExercises/FormatterEx/data.txt"));
         for(String item : data){
             String[] columns = item.split(",");
-            System.out.format("| %-8s | %8.2f | %-8s",columns[0], Double.parseDouble(columns[1]), columns[2]);
+            System.out.format("| %-8s | %-8.2f | %-12s |\n",columns[0], Double.parseDouble(columns[1]), columns[2]);
         }
     }
 }
